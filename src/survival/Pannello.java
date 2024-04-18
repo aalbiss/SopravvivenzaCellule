01d80e4fb67097580d674705ef680a4d7f58bcb0
4AI <albertoambrosi6@gmail.com>
2024-04-18 09:46:22 +0200

    Reformatted

--- src/survival/Pannello.java
+++ src/survival/Pannello.java
@@ -10,8 +10,8 @@ import java.awt.event.KeyListener;
 
 public class Pannello extends JFrame implements ActionListener, KeyListener {
     
+    final int nBottoni = 50 * 50;
     JButton[] bottoni;
-    final int nBottoni = 50*50;
     int[][] cellularStage1;
     int[][] cellularStage2;
     Timer timer;
@@ -19,7 +19,7 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
     int viciniAttivi;
     Border defaultBorder;
     
-    public Pannello(){
+    public Pannello() {
         super("Sopravvivenza Cellulare");
         setSize(900, 900);
         setLayout(new GridLayout(50, 50));
@@ -48,7 +48,7 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
             int x = (int) (Math.random() * (35 - 10 + 1)) + 10;
             int y = (int) (Math.random() * (35 - 10 + 1)) + 10;
             
-            bottoni[y*50+x].setBackground(Color.BLACK);
+            bottoni[y * 50 + x].setBackground(Color.BLACK);
             cellularStage1[x][y] = 1;
             
         }
@@ -65,33 +65,33 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
     public void actionPerformed(ActionEvent e) {
         
         
-        for (int i = 0; i < 50*50; i++) {
-            if(e.getSource() == bottoni[i]){
+        for (int i = 0; i < 50 * 50; i++) {
+            if (e.getSource() == bottoni[i]) {
                 System.out.println("Bottone premuto = " + i);
                 bottoni[i].setBorder(null);
                 bottoni[i].setBackground(Color.BLACK);
-                int x = i/50;
-                int y = i%50;
+                int x = i / 50;
+                int y = i % 50;
                 cellularStage1[x][y] = 1;
                 System.out.println(x);
                 System.out.println(y);
             }
         }
         
-        if(e.getSource() == timer){
+        if (e.getSource() == timer) {
             controlloVicini();
-            for (int i = 0; i <50; i++) {
+            for (int i = 0; i < 50; i++) {
                 for (int j = 0; j < 50; j++) {
-                    if(cellularStage2[i][j] == 0){
-                        bottoni[50*i+j].setBackground(Color.WHITE);
+                    if (cellularStage2[i][j] == 0) {
+                        bottoni[50 * i + j].setBackground(Color.WHITE);
                     } else if (cellularStage2[i][j] == 1) {
-                        bottoni[50*i+j].setBackground(Color.BLACK);
+                        bottoni[50 * i + j].setBackground(Color.BLACK);
                     } else if (cellularStage2[i][j] == -1) {
-                        bottoni[50*i+j].setBackground(Color.RED);
+                        bottoni[50 * i + j].setBackground(Color.RED);
                     } else if (cellularStage2[i][j] == 2) {
-                        bottoni[50*i+j].setBackground(Color.BLUE);
+                        bottoni[50 * i + j].setBackground(Color.BLUE);
                     }
-                    bottoni[50*i+j].setBorder(defaultBorder);
+                    bottoni[50 * i + j].setBorder(defaultBorder);
                     cellularStage2[i][j] = 0;
                 }
             }
@@ -99,28 +99,28 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
             timer2.start();
         }
         
-        if(e.getSource() == timer2) {
+        if (e.getSource() == timer2) {
             for (int i = 0; i < nBottoni; i++) {
-                if(bottoni[i].getBackground() == Color.RED){
+                if (bottoni[i].getBackground() == Color.RED) {
                     bottoni[i].setBackground(Color.WHITE);
                 } else if (bottoni[i].getBackground() == Color.BLUE) {
                     bottoni[i].setBackground(Color.BLACK);
                 }
                 
-                if(bottoni[i].getBackground() == Color.BLACK){
-                    cellularStage1[i/50][i%50] = 1;
-                }else {
-                    cellularStage1[i/50][i%50] = 0;
+                if (bottoni[i].getBackground() == Color.BLACK) {
+                    cellularStage1[i / 50][i % 50] = 1;
+                } else {
+                    cellularStage1[i / 50][i % 50] = 0;
                 }
             }
             timer.start();
             timer2.stop();
         }
         
-        if(e.getSource() == bottoni[2499]){
+        if (e.getSource() == bottoni[2499]) {
             timer.start();
         }
-        
+
 //        if(e.getSource() == bottoni[2498]){
 //            timer.stop();
 //        }
@@ -137,7 +137,7 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
 //            stampa();
 //        }
     }
-    
+
 //    public void stampa(){
 //        for (int i = 0; i < 50; i++) {
 //            for (int j = 0; j < 50; j++) {
@@ -155,7 +155,7 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
     
     @Override
     public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode() ==  KeyEvent.VK_ENTER) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
             if (!timer.isRunning()) {
                 timer.start();
                 System.out.println("PARTI");
@@ -168,25 +168,25 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
     
     }
     
-    public void controlloVicini(){
+    public void controlloVicini() {
         int xVicino;
         int yVicino;
-        for (int i = 1; i <49; i++) {
+        for (int i = 1; i < 49; i++) {
             for (int j = 1; j < 49; j++) {
                 
-                yVicino = i-1;
-                xVicino = j-1;
+                yVicino = i - 1;
+                xVicino = j - 1;
                 viciniAttivi = 0;
                 
                 for (int k = 0; k < 9; k++) {
                     //CONTARE VICINI ATTIVI
-                    if ((cellularStage1[yVicino][xVicino] == 1) && (bottoni[50*yVicino+xVicino] != bottoni[50*i+j])){
+                    if ((cellularStage1[yVicino][xVicino] == 1) && (bottoni[50 * yVicino + xVicino] != bottoni[50 * i + j])) {
                         viciniAttivi++;
                     }
-                    if(k == 2 || k == 5){
+                    if (k == 2 || k == 5) {
                         yVicino++;
                         xVicino -= 2;
-                    }else{
+                    } else {
                         xVicino++;
                     }
                 }
@@ -194,11 +194,11 @@ public class Pannello extends JFrame implements ActionListener, KeyListener {
                 //0  --> deactivated
                 //1  --> activated
                 //2  --> born
-                if(cellularStage1[i][j] == 0 && viciniAttivi == 3){
+                if (cellularStage1[i][j] == 0 && viciniAttivi == 3) {
                     cellularStage2[i][j] = 2;
                 } else if ((cellularStage1[i][j] == 1 && viciniAttivi == 2) || (cellularStage1[i][j] == 1 && viciniAttivi == 3)) {
                     cellularStage2[i][j] = 1;
-                } else if ((cellularStage1[i][j] == 1 && viciniAttivi !=  2) || (cellularStage1[i][j] == 1 && viciniAttivi != 3)) {
+                } else if ((cellularStage1[i][j] == 1 && viciniAttivi != 2) || (cellularStage1[i][j] == 1 && viciniAttivi != 3)) {
                     cellularStage2[i][j] = -1;
                 }
                 
